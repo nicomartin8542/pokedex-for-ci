3dac9cb919dd7634efb5d40e8e4e6730a2eec0ab
Nicolas <nicomartin854@gmail.com>
2022-05-11 13:22:59 -0300

    Add build and test

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: ''
+    publicPath: 'https://nicomartin8542.github.io/'
   },
   module: {
     rules: [
