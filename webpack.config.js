46ea776fd31559079c0938359a3727526b95a085
Nicolas <nicomartin854@gmail.com>
2022-05-11 13:18:18 -0300

    Add build and test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { basename } = require("path");
 const path = require('path');
 
 module.exports = {
@@ -7,8 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
-    basename: ''
+    publicPath: ''
   },
   module: {
     rules: [
